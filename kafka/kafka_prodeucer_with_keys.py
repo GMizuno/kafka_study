01e169990bdbf38fcc8320e28c25486c6eed3dd0
Gabriel Mizuno <gabrielmizuno@gmail.com>
2022-11-26 18:39:14 -0300

    Add key-partition

---
+++ kafka/kafka_prodeucer_with_keys.py
@@ -0,0 +1,75 @@
+import time
+
+from confluent_kafka import Producer
+import json
+import random
+
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        topic = msg.topic()
+        partition = msg.partition()
+        offset = msg.offset()
+        value = msg.value()
+        key = msg.key()
+        print(
+                f'Message delivered \n\ttopic = {topic}, \n\tpartition= {partition} \n\toffset = {offset} \n\tkey = {key}'
+        )
+        print(f'Message delivered with: {value}')
+        print('-' * 100)
+
+
+def calc_partiition(key):
+    if key == 'msg_1':
+        return 0
+    elif key in ('msg_2', 'msg_3', 'msg_4', 'msg_5'):
+        return 1
+    return 2
+
+
+some_data_source = [
+        {
+                'msg_1': random.randint(0, 19),
+        },
+        {
+                'msg_2': random.randint(0, 19),
+        },
+        {
+                'msg_3': random.randint(0, 19),
+        },
+        {
+                'msg_4': random.randint(0, 19),
+        },
+        {
+                'msg_5': random.randint(0, 19),
+        },
+        {
+                'msg_6': random.randint(0, 19),
+        },
+        {
+                'msg_7': random.randint(0, 19),
+        },
+        {
+                'msg_8': random.randint(0, 19),
+        },
+]
+
+for data in some_data_source:
+    p.poll(0)
+
+    m = json.dumps(data)
+    key = list(data.keys())[0]
+    p.produce(
+            'demo_python',
+            value=m,
+            key=key,
+            partition=calc_partiition(key),
+            callback=delivery_report
+    )
+    p.flush()
--- kafka/kafka_producer_with_callback.py
+++ kafka/kafka_producer_with_callback.py
@@ -1,3 +1,5 @@
+import time
+
 from confluent_kafka import Producer
 import json
 import random
@@ -15,23 +17,9 @@ def delivery_report(err, msg):
         partition = msg.partition()
         offset = msg.offset()
         value = msg.value()
-        print(f'Message delivered \n\ttopic = {topic}, \n\tnum partition= {partition} \n\toffset = {offset}')
+        print(f'Message delivered \n\ttopic = {topic}, \n\tpartition= {partition} \n\toffset = {offset}')
         print(f'Message delivered with: {value}')
-
-
-def send_data(datas: list):
-    for data in datas:
-        # Trigger any available delivery report callbacks from previous produce() calls
-        p.poll(0)
-
-        # Asynchronously produce a message. The delivery report callback will
-        # be triggered from the call to poll() above, or flush() below, when the
-        # message has been successfully delivered or failed permanently.
-        m = json.dumps(data)
-        print(m)
-        p.produce('demo_python', m, callback=delivery_report)
-
-    p.flush()
+        print('-'*100)
 
 
 some_data_source = [
@@ -60,4 +48,55 @@ some_data_source = [
                 'msg_8': random.randint(0, 19),
         },
 ]
-send_data(some_data_source)
+
+for data in some_data_source:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message. The delivery report callback will
+    # be triggered from the call to poll() above, or flush() below, when the
+    # message has been successfully delivered or failed permanently.
+    m = json.dumps(data)
+    p.produce('demo_python', m, callback=delivery_report) # Use strick-partition
+
+p.flush()
+
+
+some_data_source = [
+        {
+                'msg_1': random.randint(0, 19),
+        },
+        {
+                'msg_2': random.randint(0, 19),
+        },
+        {
+                'msg_3': random.randint(0, 19),
+        },
+        {
+                'msg_4': random.randint(0, 19),
+        },
+        {
+                'msg_5': random.randint(0, 19),
+        },
+        {
+                'msg_6': random.randint(0, 19),
+        },
+        {
+                'msg_7': random.randint(0, 19),
+        },
+        {
+                'msg_8': random.randint(0, 19),
+        },
+]
+
+for data in some_data_source:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message. The delivery report callback will
+    # be triggered from the call to poll() above, or flush() below, when the
+    # message has been successfully delivered or failed permanently.
+    m = json.dumps(data)
+    time.sleep(2)
+    p.produce('demo_python', m, callback=delivery_report) # Use round robin
+p.flush()
