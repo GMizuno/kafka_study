b1c706f47282a5d6509116cecba47141fb84ab65
Gabriel Mizuno <gabrielmizuno@gmail.com>
2022-11-26 18:09:37 -0300

    Add Callback in producer

--- kafka/kafka_producer_with_callback.py
+++ kafka/kafka_producer_with_callback.py
@@ -4,6 +4,18 @@ import random
 
 p = Producer({'bootstrap.servers': 'localhost:9092'})
 
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print(f'Message delivery failed: {err}')
+    else:
+        topic = msg.topic()
+        partition = msg.partition()
+        offset = msg.offset()
+        value = msg.value()
+        print(f'Message delivered to topic = {topic}, num partition= {partition} and offset = {offset}')
+        print(f'Message delivered with: {value}')
 
 some_data_source = [{'demo_python':'hello_world', f'{random.randint(0,100)}': random.randint(0,19)}]
 
@@ -16,6 +28,6 @@ for data in some_data_source:
     # message has been successfully delivered or failed permanently.
     m = json.dumps(data)
     print(m)
-    p.produce('demo_python', m)
+    p.produce('demo_python', m, callback=delivery_report)
 
 p.flush()
